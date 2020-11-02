53b2e12b3092c14f98362e0a8533b9e3a116bd96
Imrane-Akkouh <akkouh.imrane@gmail.com>
2020-11-02 18:31:17 +0100

    implemented add to cart front end functionnality showing on the cart dropdown

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,10 @@
 export const addItemToCart = (cartItems, cartItem) =>{
-    if(cartItems.find(item => cartItem.id === item.id)){
+    if(cartItems.find(item => cartItem.id === item.id) !== undefined){
         return cartItems.map(item=>{
             if(cartItem.id === item.id){
-                return {...cartItem, quantity: item.quantity+1}
+                return {...item , quantity: item.quantity + 1}
             }
-            return cartItem
+            return item;
         })
     }
     return [...cartItems, {...cartItem, quantity: 1}];
